9a98e41b3333ebcf668b445e70723871bc32960f
Yevhen_Ushakov <yevhen_ushakov@epam.com>
2019-12-05 11:46:10 +0200

    update content

--- js/refresh.js
+++ js/refresh.js
@@ -141,7 +141,7 @@ function hideContent(){
     $('.leaflet-control-container').show();
 }
 function isSmallWidth() {
-    return document.body.clientWidth  < 600;
+    return document.body.clientWidth  < 580;
 }
 function hideMapControls() {
     $('.leaflet-control-container').hide();
