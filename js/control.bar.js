650e3d45a1fb37d55185517e1fa2cd13776f6e61
Yevhen_Ushakov <yevhen_ushakov@epam.com>
2019-12-13 15:34:50 +0200

    fix buttons functionality

--- js/control.bar.js
+++ js/control.bar.js
@@ -7,25 +7,27 @@ L.Control.Bar = L.Control.extend({
     L.Util.setOptions(this, options);
   },
 
+  startTour: function () {
+    markerClickListener(geoJson[0], false);
+  },
+
+  showInfo: function () {
+    console.log('InfoControl')
+  },
+
   onAdd: function(map) {
     this.map = map;
     this._container = L.DomUtil.create('div', 'leaflet-control leaflet-bar');
-//    L.DomUtil.create('a', 'leaflet-social-control-'+infos[0] + ' icon-'+infos[0], this._container);
-//    L.DomUtil.create('a', 'leaflet-social-control-'+infos[0] + ' icon-'+infos[0], this._container);
-//    L.DomUtil.create('a', 'leaflet-social-control-'+infos[0] + ' icon-'+infos[0], this._container);
-    L.DomUtil.create('a', 'icon-question', this._container);
-    L.DomUtil.create('a', 'icon-map-signs', this._container);
-    /*for (var i = 0; i < this.options.links.length; i++) {
-      var infos = this.options.links[i];
-      var link = L.DomUtil.create('a', 'leaflet-social-control-'+infos[0] + ' icon-'+infos[0], this._container);
-      link.href = infos[2];
-      link.title = infos[1];
-
-      L.DomEvent
-      .addListener(link, 'click', L.DomEvent.stopPropagation)
-      .addListener(link, 'click', L.DomEvent.preventDefault)
-      .addListener(link, 'click', this.share, {self: this, link: infos[2]});
-    }*/
+    var question = L.DomUtil.create('a', 'icon-question', this._container);
+    L.DomEvent
+          .addListener(question, 'click', L.DomEvent.stopPropagation)
+          .addListener(question, 'click', L.DomEvent.preventDefault)
+          .addListener(question, 'click', this.showInfo);
+    var tour = L.DomUtil.create('a', 'icon-map-signs', this._container);
+    L.DomEvent
+          .addListener(tour, 'click', L.DomEvent.stopPropagation)
+          .addListener(tour, 'click', L.DomEvent.preventDefault)
+          .addListener(tour, 'click', this.startTour);
 
     return this._container;
   }
