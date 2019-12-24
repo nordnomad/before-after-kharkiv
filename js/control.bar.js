58cf2e35176147548f6b533946f61f94d719c551
Yevhen_Ushakov <yevhen_ushakov@epam.com>
2019-12-24 15:08:16 +0200

    change tag to prevent browser behaviour

--- js/control.bar.js
+++ js/control.bar.js
@@ -19,11 +19,13 @@ L.Control.Bar = L.Control.extend({
     this.map = map;
     this._container = L.DomUtil.create('div', 'leaflet-control leaflet-bar');
     var question = L.DomUtil.create('a', 'icon-question', this._container);
+    question.href='#';
     L.DomEvent
           .addListener(question, 'click', L.DomEvent.stopPropagation)
           .addListener(question, 'click', L.DomEvent.preventDefault)
           .addListener(question, 'click', this.showInfo);
     var tour = L.DomUtil.create('a', 'icon-map-signs', this._container);
+    tour.href='#';
     L.DomEvent
           .addListener(tour, 'click', L.DomEvent.stopPropagation)
           .addListener(tour, 'click', L.DomEvent.preventDefault)
