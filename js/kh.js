829a03093e054817a456a1f858560762e9e26cb3
Yevhen_Ushakov <yevhen_ushakov@epam.com>
2021-12-28 15:12:28 +0200

    start website on root

--- js/kh.js
+++ js/kh.js
@@ -253,6 +253,13 @@
             this.containerClasses = container.classList;
             $(this.options.containerSelector).beforeAfter();
             this._syncMaps({});
+
+            var oldParent = document.querySelector('#map-base .leaflet-control-container');
+            oldParent.remove();
+            document.querySelector('#map-overlay .leaflet-control-container').remove();
+            var newParent = document.querySelector('#my-controls');
+            newParent.appendChild(oldParent)
+
         },
 
         _initializeMarker: function (feature, latLng) {
@@ -331,16 +338,6 @@
                     var c = true
                       , d = a.dragging._draggable
                       , e = b.dragging._draggable;
-                    L.extend(b, {
-                        _tryAnimatedZoom: function(b, d, e) {
-                            c && console.log("_tryAnimatedZoom2", Date.now() - c, b, d, e);
-                            var f = L.Map.prototype._tryAnimatedZoom.call(a, b, d, e);
-                            return f
-                        },
-                        setView: function(d, e, f) {
-                            L.Map.prototype.setView.call(a,d, e, f)
-                        },
-                    });
 
                     L.extend(a, {
                         panBy: function(d, e) {
