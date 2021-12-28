403f6c438918a8ed5177f86856dbb4b0b7fb3fe0
Yevhen_Ushakov <yevhen_ushakov@epam.com>
2021-12-28 15:40:40 +0200

    start website on root

--- js/kh.js
+++ js/kh.js
@@ -339,16 +339,16 @@
                       , d = a.dragging._draggable
                       , e = b.dragging._draggable;
 
-                    L.extend(b, {
-                        _tryAnimatedZoom: function(b, d, e) {
-                            c && console.log("_tryAnimatedZoom2", Date.now() - c, b, d, e);
-                            var f = L.Map.prototype._tryAnimatedZoom.call(a, b, d, e);
-                            return f
-                        },
-                        setView: function(d, e, f) {
-                            L.Map.prototype.setView.call(a,d, e, f)
-                        },
-                    });
+//                    L.extend(b, {
+//                        _tryAnimatedZoom: function(b, d, e) {
+//                            c && console.log("_tryAnimatedZoom2", Date.now() - c, b, d, e);
+//                            var f = L.Map.prototype._tryAnimatedZoom.call(a, b, d, e);
+//                            return f
+//                        },
+//                        setView: function(d, e, f) {
+//                            L.Map.prototype.setView.call(a,d, e, f)
+//                        },
+//                    });
 
                     L.extend(a, {
                         panBy: function(d, e) {
